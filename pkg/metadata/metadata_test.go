9c66f8140980b5ea161a60462242a220d6a09d4b
Adel Ka <adel.net@gmail.com>
2025-07-19 04:12:26 +0100

    fix metadata ordered headers test

--- pkg/metadata/metadata_test.go
+++ pkg/metadata/metadata_test.go
@@ -8,9 +8,6 @@ import (
 func TestMetadataOrderedHeadersHTTP1(t *testing.T) {
 	md := &Metadata{
 		OrderedHTTP1Headers: []string{"host", "user-agent"},
-		HTTP2Frames: HTTP2FingerprintingFrames{
-			Headers: []HeaderField{{Name: "accept"}},
-		},
 	}
 	got := md.OrderedHeaders()
 	want := []string{"host", "user-agent"}
